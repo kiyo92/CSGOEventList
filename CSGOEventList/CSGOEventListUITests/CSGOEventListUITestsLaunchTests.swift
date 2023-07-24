0e8f2372958efac812664cb39cbfb135227e2f55
Joao Marcus Dionisio Araujo <ext.joao.araujo@farfetch.com>
2023-07-24 20:03:49 -0300

    Creating and setting project for viewCode

---
+++ CSGOEventList/CSGOEventListUITests/CSGOEventListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CSGOEventListUITestsLaunchTests.swift
+//  CSGOEventListUITests
+//
+//  Created by Joao Marcus Dionisio Araujo on 24/07/23.
+//
+
+import XCTest
+
+final class CSGOEventListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
